9e7f0af9baa8a107b362b1e7475a7ad69271fc0a
Sven <sven@vivaldi.com>
2020-07-13 12:00:07 -0400

    Remove useless logging code

--- grabLoot.js
+++ grabLoot.js
@@ -5,7 +5,6 @@ async function main() {
     console.log('Auto Channel Points Clicker & Point Tracker: Waiting for page to load... then grabbing...');
     await sleep(5000);
     interval_Grabber = setInterval(function() {
-        console.log("hello");
         function clickTwitchButton() {
             document.getElementsByClassName("tw-button tw-button--success tw-interactive")[0].click();
         }
